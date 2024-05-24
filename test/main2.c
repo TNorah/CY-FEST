cea65a63e80f145f9bad7f8ec36b56f574ecb1cc
TNorah <168777016+TNorah@users.noreply.github.com>
2024-05-24 02:13:07 +0200

    Update main2.c

--- test/main2.c
+++ test/main2.c
@@ -12,15 +12,20 @@ int main(){
     printf("-------------\n");
     printf("|| CY-FEST ||\n");
     printf("-------------\n");
+
+    //int ch = getchar();
+    //putchar(ch);
     
-    salles = lireSalle(&nbSalles);
+    salles = lireSalle(&nbSalles, 0);
     //printf("nbSalles = %d",nbSalles);
-    concerts = lireConcert(&nbConcerts);
+    concerts = lireConcert(&nbConcerts, 0);
     //printf("nbConcerts = %d",nbConcerts);
     
     do{
         printf("\n--MENU--\n");
-        choix = scanfD("\n1- MODE MANAGER\n2- MODE FESTIVALIER\n3- RETOUR AU MENU\n4- QUITTER LE PROGRAMME\n\n", 1);
+
+        printf("\n1- MODE MANAGER\n2- MODE FESTIVALIER\n3- RETOUR AU MENU\n4- QUITTER LE PROGRAMME\n\n");
+        choix = scanfD(1);
         
         switch(choix){
             case 1:
